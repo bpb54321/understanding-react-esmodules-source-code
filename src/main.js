1dcd23687036fd3a86b74aa99df4858168c7877d
Brian Blosser <bpb54321@gmail.com>
2025-05-20 13:35:14 -0400

    Update App to match the video React DOM Updates.

--- src/main.js
+++ src/main.js
@@ -1,11 +1,16 @@
 import { createRoot } from "../dist/react-dom/client.js";
 import { createElement } from "../dist/react/index.development.js";
 
-function App() {
-  console.log("App called");
-  return createElement("h1", null, "Hello world!");
-}
-
 const rootElement = document.getElementById("root");
 const reactRoot = createRoot(rootElement);
 reactRoot.render(createElement(App));
+
+function App() {
+  return createElement(
+    "article",
+    null,
+    createElement("h2", null, "Counter"),
+    createElement("p", null, "Button pressed 1 times"),
+    createElement("button", null, "Increment"),
+  );
+}
