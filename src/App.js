c5995392dcc0a06b3c63bec39c162718d6a4a22a
Brian Blosser <bpb54321@gmail.com>
2025-05-20 15:44:00 -0400

    Change indentation of createElement calls.

--- src/App.js
+++ src/App.js
@@ -2,10 +2,12 @@ import { createElement } from "../dist/react/index.development.js";
 import Counter from './Counter.js';
 
 export default function App() {
-    return createElement('section', null, 
+    return (
+      createElement('section', null, 
         createElement('h1', null, 'Counters'),
         createElement('section', null, 
-            createElement(Counter, null)            
+          createElement(Counter, null)            
         )
+      )
     );
 }
