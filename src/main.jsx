2610d49114e08d054694cf3ce1447134659fd60d
Brian Blosser <bpb54321@gmail.com>
2025-05-30 09:27:27 -0400

    Update jsconfig.json to point to dist since esbuild can't parse Flow files.

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,4 @@
-// import { createRoot } from "../dist/react-dom/client.js";
-import { createRoot } from "../packages/react-dom/client.js";
+import { createRoot } from "../dist/react-dom/client.js";
 import App, { state } from './App.jsx';
 
 const rootElement = document.getElementById("root");
