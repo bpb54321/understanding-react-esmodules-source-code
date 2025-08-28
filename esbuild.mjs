292a84f320cb697fc78a52e1653f60ec29e53016
Brian Blosser <bpb54321@gmail.com>
2025-08-28 09:32:30 -0400

    WIP - try just building react-dom.

--- esbuild.mjs
+++ esbuild.mjs
@@ -1,19 +1,20 @@
 import esbuild from 'esbuild';
 import babel from 'esbuild-plugin-babel'
 
-let context = await esbuild.context({
-  entryPoints: ['src/main.jsx'],
+let context = await esbuild.build({
+  entryPoints: ['packages/react-dom/client.js'],
   nodePaths: ['packages'],
   bundle: true,
   outdir: 'www/js',
   sourcemap: true,
   format: 'esm',
   target: 'esnext',
+  // jsx: 'automatic', 
   define: { __DEV__: 'false' },
   plugins: [babel()]
 }).catch(() => process.exit(1));
 
-await context.serve({
-  servedir: 'www',
-  port: 8000,
-})
+// await context.serve({
+//   servedir: 'www',
+//   port: 8000,
+// })
