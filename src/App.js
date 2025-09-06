af552c9ab0a71b1c222a7fa3587bf5e5ff35a331
Brian Blosser <bpb54321@gmail.com>
2025-09-06 16:30:39 -0400

    Adjust App.js a bit.

--- src/App.js
+++ src/App.js
@@ -6,15 +6,19 @@ function Link() {
 
 export default function App() {
   const [count, setCount] = useState(0);
+  const handleClick = () => {
+    setCount((count) => {
+      return count + 1;
+    });
+  };
+
   return (
     <div>
-      <p>
+      <div style={{marginBottom: "1rem"}}>
         <Link />
-        <br />
-        <button onClick={() => setCount((count) => count + 1)}>
-          click me - {count}
-        </button>
-      </p>
+      </div>
+      <button onClick={handleClick}>Increment</button>
+      <p>Count: {count}</p>
     </div>
   );
 }
